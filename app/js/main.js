987c894551df3fa4a3322b36db8d7ceaa43a7f0d
elkzmn <elkzmn@gmail.com>
2021-01-27 13:56:19 +0200

    added 50% aside

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,12 @@ $(function(){
     let mixer = mixitup('.product__inner-box');
 
 });
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task('style', function(){
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/normalize.css/normalize.css',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+    'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
 ])
     .pipe(gulpconcat('libs.min.css'))
     .pipe(gulpcssmin())
@@ -35,7 +36,8 @@ gulp.task('script', function(){
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'node_modules/mixitup/dist/mixitup.js',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
 ])
     .pipe(gulpconcat('libs.min.js'))
     .pipe(gulpuglify())
